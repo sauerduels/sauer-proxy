1fbae33417b5b5eb4c48a10c7d8c39847977ab82
AngrySnout <splintersal@gmail.com>
2018-06-15 01:07:55 +0300

    Fix extinfo server (resort to old method)

--- src/extinfo.cpp
+++ src/extinfo.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <map>
 #include <string.h>
 #include "extinfo.h"
 
 using std::cout;
 using std::endl;
 
+void print_buf(ENetBuffer *buf, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        cout<<std::hex<<(int)((unsigned char *)buf->data)[i]<<" ";
+    }
+    cout<<endl;
+}
 
 ExtInfoServer::ExtInfoServer(unsigned short port, unsigned int remote_host, unsigned short remote_port):
-    remote_address({remote_host, remote_port}), last_update(0)
+    remote_address({remote_host, remote_port})
 {
     pong_socket = enet_socket_create(ENET_SOCKET_TYPE_DATAGRAM);
     ENetAddress pong_address = {ENET_HOST_ANY, port};
@@ -20,35 +29,11 @@ ExtInfoServer::ExtInfoServer(unsigned short port, unsigned int remote_host, unsi
         throw "Could not create pong socket";
     enet_socket_set_option(pong_socket, ENET_SOCKOPT_NONBLOCK, 1);
 
-    ping_socket = enet_socket_create(ENET_SOCKET_TYPE_DATAGRAM);
-    ENetAddress info_address = {ENET_HOST_ANY, ENET_PORT_ANY};
-    if(ping_socket != ENET_SOCKET_NULL && enet_socket_bind(ping_socket, &info_address) < 0)
-    {
-        enet_socket_destroy(ping_socket);
-        ping_socket = ENET_SOCKET_NULL;
-    }
-    if(ping_socket == ENET_SOCKET_NULL)
-        throw "Could not create extinfo socket";
-    enet_socket_set_option(ping_socket, ENET_SOCKOPT_NONBLOCK, 1);
-
-    for (int i = 0; i < 3; i++)
-    {
-        response_bufs[i].data = new unsigned char[4096];
-        response_bufs[i].dataLength = 4096;
-    }
-
     cout<<"Extinfo server socket listening on port "<<port<<endl;
 }
 
 ExtInfoServer::~ExtInfoServer()
 {
-    for (int i = 0; i < 3; i++)
-    {
-        delete (unsigned char *)response_bufs[i].data;
-        response_bufs[i].data = NULL;
-        response_bufs[i].dataLength = 0;
-    }
-
     enet_socket_destroy(pong_socket);
     pong_socket = ENET_SOCKET_NULL;
 }
@@ -57,60 +42,45 @@ ExtInfoServer::~ExtInfoServer()
 
 void ExtInfoServer::slice(unsigned long long millis)
 {
-    update_info(millis);
     process_requests(millis);
+    process_clients(millis);
 }
 
-void ExtInfoServer::update_info(unsigned long long millis)
+void ExtInfoServer::process_clients(unsigned long long millis)
 {
     static unsigned char req[4096];
     ENetBuffer buf;
     buf.data = req;
     buf.dataLength = 4096;
 
-    if (millis-last_update >= 5000)
+    for (std::map<ENetAddress, ExtInfoClient, ENetAddressCompare>::iterator it = clients.begin(); it != clients.end(); it++)
     {
-        req[0] = 0x80;
-        req[1] = 0x01;
-        buf.dataLength = 2;
-        enet_socket_send(ping_socket, &remote_address, &buf, 1);
-
-        req[0] = 0x00;
-        req[1] = 0x01;
-        req[2] = 0xff;
-        buf.dataLength = 3;
-        enet_socket_send(ping_socket, &remote_address, &buf, 1);
-
-        req[0] = 0x00;
-        req[1] = 0x02;
-        buf.dataLength = 2;
-        enet_socket_send(ping_socket, &remote_address, &buf, 1);
-
-        last_update = millis;
-    }
-
-    buf.dataLength = 4096;
-    int len = enet_socket_receive(ping_socket, NULL, &buf, 1);
-    if (len < 0)
-        cout<<"Error: Could not receive extinfo response"<<endl;
-    else if (len >= 2)
-    {
-        if (req[0] == 0x80)
+        ExtInfoClient &client = it->second;
+        int len = enet_socket_receive(client.server_socket, NULL, &buf, 1);
+        if (len > 0)
         {
-            memcpy(response_bufs[0].data, &req[2], len-2);
-            response_bufs[0].dataLength = len-2;
+            enet_socket_send(pong_socket, &it->first, &buf, 1);
+            client.update_millis = millis;
         }
-        else if (req[1] == 0x01)
+        else if (millis - client.update_millis > 30*1000)
         {
-            memcpy(response_bufs[1].data, &req[3], len-3);
-            response_bufs[1].dataLength = len-3;
-        }
-        else if (req[1] == 0x02)
-        {
-            memcpy(response_bufs[2].data, &req[2], len-2);
-            response_bufs[2].dataLength = len-2;
+            enet_socket_destroy(it->second.server_socket);
+            it = clients.erase(it);
         }
     }
+}
+
+ENetSocket new_socket()
+{
+    ENetSocket socket = enet_socket_create(ENET_SOCKET_TYPE_DATAGRAM);
+    ENetAddress info_address = {ENET_HOST_ANY, ENET_PORT_ANY};
+    if (socket != ENET_SOCKET_NULL && enet_socket_bind(socket, &info_address) < 0)
+    {
+        enet_socket_destroy(socket);
+        return(ENET_SOCKET_NULL);
+    }
+    enet_socket_set_option(socket, ENET_SOCKOPT_NONBLOCK, 1);
+    return socket;
 }
 
 void ExtInfoServer::process_requests(unsigned long long millis)
@@ -123,13 +93,19 @@ void ExtInfoServer::process_requests(unsigned long long millis)
     int len = enet_socket_receive(pong_socket, &pong_address, &buf, 1);
     if (len <= 0 || len > 4096) return;
 
-    ENetBuffer *response_buf;
-    if (pong[0] != 0x00) response_buf = &response_bufs[0];
-    else if (pong[1] == 0x01) response_buf = &response_bufs[1];
-    else if (pong[1] == 0x02) response_buf = &response_bufs[2];
+    ENetSocket ping_socket;
+    std::map<ENetAddress, ExtInfoClient, ENetAddressCompare>::iterator it = clients.find(pong_address);
+    if (it == clients.end())
+    {
+        ping_socket = new_socket();
+        clients[pong_address] = ExtInfoClient{ping_socket, millis};
+    }
+    else
+    {
+        ping_socket = it->second.server_socket;
+        it->second.update_millis = millis;
+    }
 
-    int replyLen = std::min(4096-len, (int)response_buf->dataLength);
-    memcpy(&pong[len], response_buf->data, replyLen);
-    buf.dataLength = len + replyLen;
-    enet_socket_send(pong_socket, &pong_address, &buf, 1);
+    buf.dataLength = len;
+    len = enet_socket_send(ping_socket, &remote_address, &buf, 1);
 }
--- src/extinfo.h
+++ src/extinfo.h
@@ -1,4 +1,19 @@
+#include <map>
 #include <enet/enet.h>
+
+struct ExtInfoClient {
+    ENetSocket server_socket;
+    unsigned long long update_millis;
+};
+
+struct ENetAddressCompare
+{
+   bool operator() (const ENetAddress& lhs, const ENetAddress& rhs) const
+   {
+       if (lhs.host == rhs.host) return lhs.port < rhs.port;
+       return lhs.host < rhs.host;
+   }
+};
 
 class ExtInfoServer
 {
@@ -6,13 +21,11 @@ public:
     ExtInfoServer(unsigned short port, unsigned int remote_host, short unsigned remote_port);
     ~ExtInfoServer();
     void slice(unsigned long long millis);
-    void update_info(unsigned long long millis);
+    void process_clients(unsigned long long millis);
     void process_requests(unsigned long long millis);
 
 private:
     ENetSocket pong_socket;
-    ENetSocket ping_socket;
     ENetAddress remote_address;
-    unsigned long long last_update;
-    ENetBuffer response_bufs[3];
+    std::map<ENetAddress, ExtInfoClient, ENetAddressCompare> clients;
 };
